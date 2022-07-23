930dbb015b8cef2485425a3eb479f0832d04fe54
ryne <5797666+remotelyqueued@users.noreply.github.com>
2022-07-22 23:27:31 -0700

    Initial setup

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,22 @@
 // main.js
+
+const container = document.createElement('div');
+container.setAttribute('id', 'container');
+container.classList.add('container');
+
+for (let i = 0; i < 256; i++) {
+    let div = document.createElement('div');
+    container.append(div);
+}
+
+document.body.append(container);
+
+function createGrid(size) {}
+
+function clearGrid() {}
+
+container.addEventListener('mouseover', event => {
+    console.log(event.target);
+    event.target.classList.add('color');
+    event.target.style.backgroundColor = 'blue';
+});
