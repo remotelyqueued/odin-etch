54208cadbf1279f721d94be2e828c9f3c75062b9
remotelyqueued <5797666+remotelyqueued@users.noreply.github.com>
2022-07-23 23:50:18 -0700

    Finished general functionality

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # odin-etch
 
+todo: 
+color change
+user input
+ui design
+color gradient
--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,30 @@
 // main.js
 
+// https://developer.mozilla.org/en-US/docs/Web/CSS/Using_CSS_custom_properties
+
+// max input 100
 const container = document.createElement('div');
 container.setAttribute('id', 'container');
-container.classList.add('container');
 
-for (let i = 0; i < 256; i++) {
-    let div = document.createElement('div');
-    container.append(div);
+let size = 100;
+
+createGrid(size);
+drawGrid();
+
+function createGrid(size) {
+    const totalSquares = size * size;
+    for (let i = 0; i < totalSquares; i++) {
+        let div = document.createElement('div');
+        container.append(div);
+    }
 }
 
-document.body.append(container);
-
-function createGrid(size) {}
-
-function clearGrid() {}
+function drawGrid() {
+    let root = document.querySelector(':root');
+    root.style.setProperty('--grid', size);
+    document.body.append(container);
+}
 
 container.addEventListener('mouseover', event => {
-    console.log(event.target);
-    event.target.classList.add('color');
-    // event.target.style.backgroundColor = 'blue';
+    event.target.style.backgroundColor = 'blue';
 });
