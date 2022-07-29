b43535f5789994b5fbc3ac0014c385dab22c5773
ryne <5797666+remotelyqueued@users.noreply.github.com>
2022-07-29 01:24:03 -0700

    Minor changes

--- scripts/main.js
+++ scripts/main.js
@@ -20,24 +20,26 @@ const range = document.getElementById('range');
 const grid = document.getElementById('grid');
 const root = document.documentElement;
 
-const set = document.getElementById('set');
+const setButton = document.getElementById('setButton');
 
 let container = document.createElement('div');
 
 container.setAttribute('id', 'container');
-container.append(...createGrid(size));
 
+container.append(...createGrid(size));
 drawGrid();
+
+// createRandomGrid(size);
 
 rangeDisplay.textContent = size;
 
-container.addEventListener('mouseover', changeColor);
+container.addEventListener('pointerover', changeColor);
 
 range.addEventListener('change', event => {
     rangeDisplay.textContent = event.target.value;
 });
 
-set.addEventListener('click', event => {
+setButton.addEventListener('click', event => {
     size = Number(range.value);
 
     container.remove();
@@ -46,13 +48,13 @@ set.addEventListener('click', event => {
 
     if (optionSwitcher.value === 'gradient') {
         container.append(...createGrid(size, true));
-        container.addEventListener('mouseover', changeGradient);
+        container.addEventListener('pointerover', changeGradient);
     } else if (optionSwitcher.value === 'colors') {
         container.append(...createGrid(size));
-        container.addEventListener('mouseover', changeColor);
+        container.addEventListener('pointerover', changeColor);
     } else {
         container.append(...createGrid(size));
-        container.addEventListener('mouseover', changeRandom);
+        container.addEventListener('pointerover', changeRandom);
         colorSwitcher.setAttribute('disabled', true);
     }
     drawGrid();
@@ -65,7 +67,7 @@ colorSwitcher.addEventListener('change', event => {
 optionSwitcher.addEventListener('change', event => {
     // ...
     // how to know which event listeners are on element?
-    // container.removeEventListener('mouseover', changeColor);
+    // container.removeEventListener('pointerover', changeColor);
 
     // id, name isn't unique
     // const clone = container.cloneNode(true);
@@ -76,15 +78,15 @@ optionSwitcher.addEventListener('change', event => {
 
     if (event.target.value == 'gradient') {
         container.append(...createGrid(size, true));
-        container.addEventListener('mouseover', changeGradient);
+        container.addEventListener('pointerover', changeGradient);
         colorSwitcher.setAttribute('disabled', true);
     } else if (event.target.value == 'colors') {
         container.append(...createGrid(size));
-        container.addEventListener('mouseover', changeColor);
+        container.addEventListener('pointerover', changeColor);
         colorSwitcher.removeAttribute('disabled');
     } else {
         container.append(...createGrid(size));
-        container.addEventListener('mouseover', changeRandom);
+        container.addEventListener('pointerover', changeRandom);
         colorSwitcher.setAttribute('disabled', true);
     }
     drawGrid();
@@ -145,3 +147,63 @@ function changeRandom(event) {
         }
     }
 }
+
+export function createRandomGrid(size) {
+    let grid = [];
+
+    let hexNumbers = [
+        '0',
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+        'A',
+        'B',
+        'C',
+        'D',
+        'E',
+        'F',
+    ];
+
+    const totalSquares = size * size;
+
+    for (let i = 0; i < totalSquares; i++) {
+        let div = document.createElement('div');
+        let hexColorCode = '';
+        for (let i = 0; i < 6; i++) {
+            let randomIndex = Math.floor(Math.random() * hexNumbers.length);
+            hexColorCode += hexNumbers[randomIndex];
+        }
+        div.style.backgroundColor = '#' + hexColorCode;
+        grid.push(div);
+    }
+
+    container.append(...grid);
+    drawGrid();
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(
+        document.querySelectorAll('.navbar-burger'),
+        0
+    );
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach(el => {
+        el.addEventListener('click', () => {
+            // Get the target from the "data-target" attribute
+            const target = el.dataset.target;
+            const $target = document.getElementById(target);
+
+            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+            el.classList.toggle('is-active');
+            $target.classList.toggle('is-active');
+        });
+    });
+});
