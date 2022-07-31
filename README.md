cab7b22c9cf29709ab10fa41549a5724b977f1d7
ryne <5797666+remotelyqueued@users.noreply.github.com>
2022-07-30 18:27:13 -0700

    Used a different method to produce random colors, fixed readme

--- README.md
+++ README.md
@@ -1,6 +1,50 @@
 # odin-etch
 
-todo:
-fix readme
+sites I relied on
 
-https://remotelyqueued.github.io/odin-etch/
+-   [odin-project](https://www.theodinproject.com/dashboard)
+-   [javascript-info](https://javascript.info/)
+-   [mdn](https://developer.mozilla.org/en-US/)
+-   [airbnb style guide](https://github.com/airbnb/javascript)
+
+-   [bulma.io](https://bulma.io/)
+
+site link: https://remotelyqueued.github.io/odin-etch/
+
+# Concepts
+
+## HTML
+
+-   semantic elements
+-   accessibility
+
+## CSS
+
+-   frameworks
+-   grid
+-   flexbox
+-   classes
+-   pseudo classes
+-   variables
+-   webfonts
+
+## JavaScript
+
+-   resource loading
+-   modules
+-   data types
+-   variables
+-   arrays
+-   functions
+-   scope
+-   closure
+-   return values
+-   conditonals
+-   loops
+-   events
+-   DOM manipulation
+
+## Git
+
+-   branching
+-   merging
--- scripts/randomColor.js
+++ scripts/randomColor.js
@@ -1,4 +1,4 @@
-const hexNumbers = [
+/* const hexNumbers = [
     '0',
     '1',
     '2',
@@ -15,18 +15,25 @@ const hexNumbers = [
     'D',
     'E',
     'F',
-];
+]; */
 
 /**
  * generateRandomColor() returns a random 6 digit hex color
  *
+ * firefox complains if the hex number isn't 6 digits
+ * couple different ways to do this: found online
+ *
  * @returns {String} hexColorCode
  */
 export function generateRandomColor() {
-    let hexColorCode = '';
-    for (let i = 0; i < 6; i++) {
-        const randomIndex = Math.floor(Math.random() * hexNumbers.length);
-        hexColorCode += hexNumbers[randomIndex];
-    }
-    return hexColorCode;
+    // let hexColorCode = '';
+    // for (let i = 0; i < 6; i++) {
+    //     const randomIndex = Math.floor(Math.random() * hexNumbers.length);
+    //     hexColorCode += hexNumbers[randomIndex];
+    // }
+    // return hexColorCode;
+
+    return Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, '0');
 }
